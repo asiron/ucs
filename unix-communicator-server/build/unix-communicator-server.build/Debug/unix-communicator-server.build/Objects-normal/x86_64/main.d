94251d477414300a6892bb071080420f2d713a5a
Maciej Żurad <maciej.zurad@gmail.com>
2013-01-26 14:35:32 +0100

    Adding file structure

---
+++ unix-communicator-server/build/unix-communicator-server.build/Debug/unix-communicator-server.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/asiron/Dropbox/Studies/SOP2/SOP2-project/ucs/unix-communicator-server/src/main.c
---
+++ unix-communicator-server/src/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  unix-communicator-server
+//
+//  Created by Maciej Żurad on 1/26/13.
+//  Copyright (c) 2013 Maciej Żurad. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+
+    return 0;
+}
+
